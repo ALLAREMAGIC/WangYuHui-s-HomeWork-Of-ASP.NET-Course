9042f6fea152c2f24c75b6c131b0d100537b5154
MAGICCOMPUTER\Magic Computer <710534813@qq.com>
2022-11-23 14:15:13 +0800

    2022.11.23.1

---
+++ ASP.NET TEST/Pages/C7/LinqDSGrid.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASP.NET_TEST.Pages.C7
+{
+    public partial class LinqDSGrid : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
